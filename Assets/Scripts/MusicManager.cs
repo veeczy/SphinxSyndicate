be31ae04932bea0571f5fbe9b450cc8e1205ff80
walkermarc544 <81835163+walkermarc544@users.noreply.github.com>
2025-10-06 01:58:58 -0400

    Player Movement
    - Playable Scenes
    - Background Music Manager
    - Player Movement Script

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D myPlayer;
+    public float speed = 1.0f;
+    public float stopSpeed = 1.0f;
+    public Vector2 aimPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myPlayer.linearDamping = stopSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MOVEMENT
+        
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction.Normalize();
+            myPlayer.linearVelocity = direction * speed;
+        
+        //AIM
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        if (hit.collider != null)
+        {
+            aimPos = new Vector2(hit.point.x, hit.point.y);
+        }
+       else
+       {
+           Debug.Log("OUT OF BOUNDS");
+       }
+       Vector2 targetRot = mousePos - (Vector2)transform.position;
+       float angle = Mathf.Atan2(targetRot.y, targetRot.x) * Mathf.Rad2Deg;
+       transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
