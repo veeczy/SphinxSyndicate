9dcf565dc735c350e3afe92c64a86216d1d8b5ff
DylanJaBr <dylan1br2@gmail.com>
2026-02-08 14:51:24 -0500

    Revert "Merge branch 'main' into CityLevels"
    This reverts commit 728fa99e6297e7f1aec6551fc9daa4959db0c03e, reversing
    changes made to 9853f5061ba6241f796857200c51bc653e342bb4.

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [Header("Trigger Settings")]
+    public bool onTrigger = true;
+
+    [Header("Is this a secret door?")]
+    public bool isSecretDoor = false;
+
+    [Header("Force Scene Index (1 or 2 overrides randomizer)")]
+    public int forcedSceneIndex = -1;  // NEW
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!onTrigger) return;
+        if (!other.CompareTag("Player")) return;
+
+        // NEW: if this door should send player to scene 1 or 2
+        if (forcedSceneIndex == 1 || forcedSceneIndex == 2)
+        {
+            LevelManager.instance.LoadSceneByTrigger(forcedSceneIndex);
+            return;
+        }
+
+        if (isSecretDoor)
+        {
+            LevelManager.instance.LoadSecretRoom();
+        }
+        else
+        {
+            LevelManager.instance.LoadNextRoom();
+        }
+    }
+}
+
+
+
+
+
