b2979c80c7cf5b27e24dc77d87f6baede9e2ca3f
Vee Czyscon <ma191433@ucf.edu>
2026-02-09 23:03:48 -0500

    BlackJack Tune Up, Boss Counter in Player Prefs

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -6,15 +6,21 @@ public class PlayerPrefsManager : MonoBehaviour
     public int playerHealth;
     public int playerMaxHealth = 3;
 
+    //variables for updating progress in game
+    public int bossCounter;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PlayerPrefs.SetInt("health", playerMaxHealth); //at start player prefs memory of health is reset to max health
+
+        PlayerPrefs.SetInt("bossCounter", 0); //reset amount of bosses beaten to 0
     }
 
     // Update is called once per frame
     void Update()
     {
         playerHealth = PlayerPrefs.GetInt("health");
+        bossCounter = PlayerPrefs.GetInt("bossCounter");
     }
 }
